dfb99b5c3f8c3075d39ee9031e864b03b718ba87
StarPluckerZ <StarPluckerZ@protonmail.com>
2023-01-03 18:58:18 +0800

    Add hangfire support

---
+++ MoeUC/MoeUC.Api/StartUps/HangfireStartup.cs
@@ -0,0 +1,32 @@
+﻿using Hangfire;
+using Hangfire.InMemory;
+using MoeUC.Core.Infrastructure.StartupConfigs;
+
+namespace MoeUC.Api.StartUps;
+
+public class HangfireStartup : IMoeStartup
+{
+    public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHangfire(c => c.UseInMemoryStorage(new InMemoryStorageOptions()
+        {
+            DisableJobSerialization = true
+        }));
+
+        services.AddHangfireServer();
+    }
+
+    public void Configure(IApplicationBuilder application)
+    {
+        application.UseHangfireDashboard();
+
+        // Add RecurringJob Here
+    }
+
+    public int Order => 9999;
+}
+
+public class RecurringJobService
+{
+
+}
