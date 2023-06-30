93653bcbe972e94f1adce0a7b69a500544a1b054
StarPluckerZ <StarPluckerZ@protonmail.com>
2023-06-30 11:43:58 +0800

    Migrate to Postgresql

--- MoeUC/MoeUC.Api/StartUps/DbStartup.cs
+++ MoeUC/MoeUC.Api/StartUps/DbStartup.cs
@@ -8,20 +8,9 @@ public class DbStartup : IMoeStartup
 {
     public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
-        var dbms = configuration.GetSection("DBMS").Value;
-        if (!string.IsNullOrWhiteSpace(dbms))
-        {
-            if (dbms.Equals("SQLServer", StringComparison.InvariantCultureIgnoreCase))
-                services.AddDbContext<MoeDbContext>(options =>
-                    options.UseSqlServer(configuration.GetConnectionString("MoeUC")));
-            if (dbms.Equals("MySQL", StringComparison.InvariantCultureIgnoreCase))
-                services.AddDbContext<MoeDbContext>(options =>
-                    options.UseMySQL(configuration.GetConnectionString("MoeUC")!));
-        }
-        else
-            throw new Exception("Must specify DBMS name.");
-
-
+        
+        services.AddDbContext<MoeDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("MoeUC")));
     }
 
     public void Configure(IApplicationBuilder application)
