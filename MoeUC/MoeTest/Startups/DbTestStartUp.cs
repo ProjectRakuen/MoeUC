4aa50e0e8a2321e0efadc809236cbaed7941c546
StarPluckerZ <StarPluckerZ@protonmail.com>
2023-07-02 10:40:57 +0800

    pass tests

--- MoeUC/MoeTest/Startups/DbTestStartUp.cs
+++ MoeUC/MoeTest/Startups/DbTestStartUp.cs
@@ -9,18 +9,8 @@ public class DbTestStartUp : IMoeTestStartup
 {
     public void ConfigureService(IServiceCollection services, IConfiguration configuration)
     {
-        var dbms = configuration.GetSection("DBMS").Value;
-        if (!string.IsNullOrWhiteSpace(dbms))
-        {
-            if (dbms.Equals("SQLServer", StringComparison.InvariantCultureIgnoreCase))
-                services.AddDbContext<MoeDbContext>(options =>
-                    options.UseSqlServer(configuration.GetConnectionString("MoeUC")));
-            if (dbms.Equals("MySQL", StringComparison.InvariantCultureIgnoreCase))
-                services.AddDbContext<MoeDbContext>(options =>
-                    options.UseMySQL(configuration.GetConnectionString("MoeUC")!));
-        }
-        else
-            throw new Exception("Must specify DBMS name.");
+        services.AddDbContext<MoeDbContext>(options => 
+            options.UseNpgsql(configuration.GetConnectionString("MoeUC")!));
     }
 
     public int Order => 0;
